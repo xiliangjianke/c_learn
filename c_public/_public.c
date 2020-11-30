019633ee47c494f1025cc9b34fa53d7eb7051d8a
neko <biankairui@163.com>
2020-11-30 14:52:48 +0800

    public function include min and max

---
+++ c_public/_public.c
@@ -0,0 +1,23 @@
+/*
+ * programname: _public.h, function file of common function declaration
+ * author: neko date:20201130
+*/
+
+#include "_public.h" 
+
+// get smaller integer of two integers
+int min(const int ii1,const int 112)
+{
+	if (ii1 < ii2) return ii1;
+
+	return ii2;
+}
+
+// get bigger integer of two integers
+int max(const int ii1,const int 112)
+{
+	if (ii1 > ii2) return ii1;
+
+	return ii2;
+}
+
---
+++ c_public/_public.h
@@ -0,0 +1,16 @@
+/*
+ * programname: _public.h, header file of common function declaration
+ * author: neko date:20201130
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// declare min function, get smaller integer of two integers
+int min(const int ii1,const int 112);
+
+
+// declare max function, get bigger integer of two integers
+int max(const int ii1,const int 112);
+

be2f59eb7ee3381e5b23c42b54cc336ed0d9a030
neko <biankairui@163.com>
2020-12-05 20:52:58 +0800

    add string-reletated function

--- _public.c
+++ _public.c
@@ -24,7 +24,9 @@ int max(const int ii1,const int ii2)
 // return int of char
 int ctoi(const char chr)
 {
-	switch (chr)
+	if ((chr >= '0') || (chr <= '9')) return chr-'0';
+	return -1;
+	/*switch (chr)
 	{
 		case '0': return 0;
 		case '1': return 1;
@@ -37,5 +39,143 @@ int ctoi(const char chr)
 		case '8': return 8;
 		case '9': return 9;
 		default: return -1;
+	}*/
+}
+
+
+// safe strcpy function: copy src to dest
+char *STRCPY(char* dest,const size_t dest_size, const char* src)
+{
+	if (dest == 0) return 0; // if dest if empty string,return 0
+	memset(dest,0,sizeof(dest)); // initialize dest
+
+	if (src == 0) return dest; // special case: src is empty string
+	
+	// if len(src) > dest_size?strncpy:srcpy
+	if (strlen(src) > dest_size - 1) { strncpy(dest, src, dest_size - 1); }
+	else strcpy(dest, src);
+
+	return dest;
+}
+
+
+// safe strcpy function: copy src to dest
+char *STRNCPY(char* dest,const size_t dest_size, const char* src, size_t n)
+{
+	if (dest == 0) return 0; // if dest if empty string,return 0
+	memset(dest,0,sizeof(dest)); // initialize dest
+
+	if (src == 0) return dest; // special case: src is empty string
+	
+	// if len(src) > dest_size?strncpy:srcpy
+	if (n > dest_size - 1) { strncpy(dest, src, dest_size - 1); }
+	else strncpy(dest, src, n);
+
+	return dest;
+	
+}
+
+
+// safe strcat function
+char *STRCAT(char* dest,const size_t dest_size, const char* src)
+{
+	if (dest == 0) return 0;	
+	if (src == 0) return dest;	
+
+	unsigned int free_len = dest_size - strlen(dest) - 1;
+
+	if (free_len > strlen(src)) strcat(dest, src);
+	else { strncat(dest, src, free_len); dest[dest_size - 1] = 0; } 
+
+	return dest;
+}
+
+// safe strcat function
+char *STRNCAT(char* dest,const size_t dest_size, const char* src, const size_t n)
+{
+	if (dest == 0) return 0;	
+	if (src == 0) return dest;	
+
+	unsigned int free_len = dest_size - strlen(dest) - 1;
+
+	if (free_len > n) strncat(dest, src, n);
+	else { strncat(dest, src, free_len); dest[dest_size - 1] = 0; } 
+
+	return dest;
+}
+
+
+// delete string to the right of specified char
+void DeleteRChar(char *str,const char chr)
+{
+	if (str == 0) return;	
+	if (strlen(str) == 0) return;	
+
+	int str_len = strlen(str);
+
+	while (str_len > 0)
+	{
+		if (str[str_len - 1] == chr) break;
+		str[str_len - 1]=0;
+		str_len--;
 	}
 }
+
+// delete string to the left of specified char
+void DeleteLChar(char *str,const char chr)
+{
+	if (str == 0) return;
+	if (strlen(str) == 0) return;
+
+	char temp_str[strlen(str) + 1];
+	memset(temp_str, 0, sizeof(temp_str));
+	strcpy(temp_str, str);
+
+	int temp_index = 0;
+
+	while (temp_str[temp_index] != chr) temp_index++;
+
+	memset(str, 0, sizeof(str));
+	strcpy(str, temp_str + temp_index);
+	
+}
+
+// delete string to the left and right of specified char
+void DeleteLRChar(char *str,const char chr)
+{
+	DeleteLChar(str, chr);	
+	DeleteRChar(str, chr);	
+}
+
+// convert lowercase to uppercase,ignore non alphabetic type
+void ToUpper(char *str)
+{
+	if (str == 0) return;
+	if (strlen(str) == 0) return;
+
+	int len_str = strlen(str);
+	int ii = 0;
+
+	for (ii=0;ii<len_str;ii++)
+	{
+		if ((str[ii] >= 'a') && (str[ii] <= 'z')) str[ii] -= 32;	
+	}
+}
+//void ToUpper(string &str)
+
+// convert uppercase to lowercase,ignore non alphabetic type
+void ToLower(char *str)
+{
+	if (str == 0) return;
+	if (strlen(str) == 0) return;
+
+	int len_str = strlen(str);
+	int ii = 0;
+
+	for (ii=0;ii<len_str;ii++)
+	{
+		if ((str[ii] >= 'A') && (str[ii] <= 'Z')) str[ii] += 32;	
+	}
+}
+
+//void ToLower(string &str)
--- _public.h
+++ _public.h
@@ -15,5 +15,60 @@ int min(const int ii1,const int ii2);
 int max(const int ii1,const int ii2);
 
 // declare ctoi function, return int of char 
-// return 0 if input is not a integer
+// return 0 if input is not a integeri
 int ctoi(const char chr);
+
+// safe strcpy function: copy src to dest
+// dest: target string which will be initialized in
+// dest_szie: memory size of dest string
+// src: source string
+// return value: adress of dest string
+char *STRCPY(char* dest,const size_t dest_size, const char* src);
+
+// safe strcpy function: copy src to dest
+// dest: target string which will be initialized in
+// dest_szie: memory size of dest string
+// src: source string
+// n: number of bytes for copying 
+// return value: adress of dest string
+char *STRNCPY(char* dest,const size_t dest_size, const char* src, size_t n);
+
+// safe strcat function
+// dest: target string which will be initialized in
+// dest_szie: memory size of dest string
+// src: source string
+// return value: adress of dest string
+char *STRCAT(char* dest,const size_t dest_size, const char* src);
+
+// safe strcat function
+// dest: target string which will be initialized in
+// dest_szie: memory size of dest string
+// src: source string
+// n: number of bytes for copying 
+// return value: adress of dest string
+char *STRNCAT(char* dest,const size_t dest_size, const char* src, const size_t n);
+
+// delete specified char at the right of string
+// str: target string
+// chr: char to be deleted
+void DeleteRChar(char *str,const char chr);
+
+// delete specified char at the left of string
+// str: target string
+// chr: char to be deleted
+void DeleteLChar(char *str,const char chr);
+
+// delete specified char on the left and right side of string
+// str: target string
+// chr: char to be deleted
+void DeleteLRChar(char *str,const char chr);
+
+// convert lowercase to uppercase,ignore non alphabetic type
+// str: string to be converted,char and string type are suppported
+void ToUpper(char *str);
+//void ToUpper(string &str);
+
+// convert uppercase to lowercase,ignore non alphabetic type
+// str: string to be converted,char and string type are suppported
+void ToLower(char *str);
+//void ToLower(string &str);
